f230c2f07f31c2e5deacb503fe83aac019944098
WeDox <1986821027@qq.com>
2018-07-09 11:03:01 +0800

    环信单一聊天页面

---
+++ app/src/main/java/kangmei/com/hxchart/MainActivity.java
@@ -0,0 +1,14 @@
+package kangmei.com.hxchart;
+
+import android.os.Bundle;
+
+import com.hyphenate.easeui.ui.EaseBaseActivity;
+
+public class MainActivity extends EaseBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
